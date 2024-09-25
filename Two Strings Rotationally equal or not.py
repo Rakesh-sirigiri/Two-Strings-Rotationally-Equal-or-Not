13479f92d7df3948436775de7787ed702a0d1be5
Sirigiri Rakesh <94678816+Rakesh-sirigiri@users.noreply.github.com>
2024-09-25 11:16:33 +0530

    Add files via upload

---
+++ Two Strings Rotationally equal or not.py
@@ -0,0 +1,6 @@
+str1 = 'Rakesh'
+str2 = 'heskaR'
+print("The original string 1 is : " + str(str1))
+print("The original string 2 is : " + str(str2))
+res = str2 in (str1+str2)
+print("Are two strings Rotationally equal ? : " + str(res))
